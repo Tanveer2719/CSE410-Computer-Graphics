a9131eb4bb33bdf77f3b64a60352b2d43c5144a7
Tanveer <1905025@ugrad.cse.buet.ac.bd>
2023-12-14 15:06:31 +0600

    task2 finished

--- Offline 1/play.cpp
+++ Offline 1/play.cpp
@@ -7,9 +7,10 @@
 int drawgrid;
 int drawaxes;
 
-double triangle_edge_len ;  // length of an edge of a triangle
-double max_sphere_radius;    // maximum radius of a sphere, depends on edgeLength
-double current_sphere_radius;    // current sphere radius, initially set to 0 and incremented 
+double edge_len ;  // length of an edge of a triangle
+double max_edge_len;    // maximum length of an edge
+double max_radius;    // maximum radius of a sphere, depends on edgeLength
+double current_radius;    // current sphere radius, initially set to 0 and incremented 
 double increment_radius;        // measure to increment the current sphere radius
 
 Camera* camera;
@@ -39,39 +40,46 @@ void drawAxes()
 void drawGrid()
 {
 	int i;
-    glColor3f(0.6, 0.6, 0.6);	//grey
-    glBegin(GL_LINES);{
-        for(i=-18;i<=18;i++){
+	if(drawgrid==1)
+	{
+		glColor3f(0.6, 0.6, 0.6);	//grey
+		glBegin(GL_LINES);{
+			for(i=-8;i<=8;i++){
 
-            if(i==0)
-                continue;	//SKIP the MAIN axes
+				if(i==0)
+					continue;	//SKIP the MAIN axes
 
-            //lines parallel to Y-axis
-            glVertex3f(i*0.1, -1.9, 0);
-            glVertex3f(i*0.1,  1.9, 0);
+				//lines parallel to Y-axis
+				glVertex3f(i*10, -90, 0);
+				glVertex3f(i*10,  90, 0);
 
-            //lines parallel to X-axis
-            glVertex3f(-1.9, i*0.1, 0);
-            glVertex3f( 1.9, i*0.1, 0);
-        }
-    }glEnd();
-
+				//lines parallel to X-axis
+				glVertex3f(-90, i*10, 0);
+				glVertex3f( 90, i*10, 0);
+			}
+		}glEnd();
+	}
 }
 
-void drawTriangle(){
+void drawTriangle(double l){
     glBegin(GL_TRIANGLES);{
-        glVertex3f(1,0,0);
-        glVertex3f(0,1,0);
-        glVertex3f(0,0,1);
+        glVertex3f(l,0,0);
+        glVertex3f(0,l,0);
+        glVertex3f(0,0,l);
     }glEnd();
 }
 
 void drawTriangles(){
+	double d = max_edge_len - edge_len;
+	d /=3.0;
+
+
     for(int i = 0; i<4; i++){
         glPushMatrix();{
             glColor3f((i+1)%2, i%2, 1);
             glRotatef(i*90, 0, 1, 0);
-            drawTriangle();
+			glTranslatef(d, d, d);
+            drawTriangle(edge_len);
         }glPopMatrix(); 
     }
 
@@ -80,96 +88,57 @@ void drawTriangles(){
             glColor3f((i)%2, (i+1)%2, 1);
             glRotatef(i*90, 0, 1, 0);
             glRotatef(180, 1, 0, 1);
-            drawTriangle();
+			glTranslatef(d,d,d);
+            drawTriangle(edge_len);
         }glPopMatrix(); 
     }
 }
 
-void drawSquare(double a)
-{
-    //glColor3f(1.0,0.0,0.0);
+void drawCylinderPart(double radius, double height, int segments){
+	Vector3D points[segments+1];
+	double phi = 70.5287794 * pi/180;		// in radians
+
+	for(int i = 0; i<segments+1; i++){
+		double theta = -phi/2 + i * phi/(segments-1);  // spread the points from -phi/2 to phi/2
+		points[i].x = radius * cos(theta);
+		points[i].y = radius * sin(theta);
+	}
+
 	glBegin(GL_QUADS);{
-		glVertex3f( a, a,2);
-		glVertex3f( a,-a,2);
-		glVertex3f(-a,-a,2);
-		glVertex3f(-a, a,2);
+		for(int i = 0; i<segments; i++){
+			glVertex3f(points[i].x, points[i].y, height/2);
+			glVertex3f(points[i].x, points[i].y, -height/2);
+			glVertex3f(points[i+1].x, points[i+1].y, -height/2);
+			glVertex3f(points[i+1].x, points[i+1].y, height/2);
+		}
 	}glEnd();
+
 }
 
-void drawCircle(double radius,int segments)
+void drawSpherePart(double radius,int slices )
 {
-    int i;
-    Vector3D points[100];
-    glColor3f(0.7,0.7,0.7);
-    //generate points
-    for(i=0;i<=segments;i++)
-    {
-        points[i].x=radius*cos(((double)i/(double)segments)*2*pi);
-        points[i].y=radius*sin(((double)i/(double)segments)*2*pi);
-    }
-    //draw segments using generated points
-    for(i=0;i<segments;i++)
-    {
-        glBegin(GL_LINES);
-        {
-			glVertex3f(points[i].x,points[i].y,0);
-			glVertex3f(points[i+1].x,points[i+1].y,0);
-        }
-        glEnd();
-    }
-}
-
-void drawCone(double radius,double height,int segments)
-{
-    int i;
-     double shade;
-    Vector3D points[100];
-    //generate points
-    for(i=0;i<=segments;i++)
-    {
-        points[i].x=radius*cos(((double)i/(double)segments)*2*pi);
-        points[i].y=radius*sin(((double)i/(double)segments)*2*pi);
-    }
-    //draw triangles using generated points
-    for(i=0;i<segments;i++)
-    {
-        //create shading effect
-        if(i<segments/2)shade=2*(double)i/(double)segments;
-        else shade=2*(1.0-(double)i/(double)segments);
-        glColor3f(shade,shade,shade);
-
-        glBegin(GL_TRIANGLES);
-        {
-            glVertex3f(0,0,height);
-			glVertex3f(points[i].x,points[i].y,0);
-			glVertex3f(points[i+1].x,points[i+1].y,0);
-        }
-        glEnd();
-    }
-}
-
-void drawSphere(double radius,int slices,int stacks)
-{
-	Vector3D points[100][100];
-	int i,j;
-	double h,r;
+	struct Vector3D points[slices+1][slices+1];
 	//generate points
-	for(i=0;i<=stacks;i++)
+	for(int i=0;i<=slices;i++)
 	{
-		h=radius*sin(((double)i/(double)stacks)*(pi/2));
-		r=radius*cos(((double)i/(double)stacks)*(pi/2));
-		for(j=0;j<=slices;j++)
+		for(int j=0;j<=slices;j++)
 		{
-			points[i][j].x=r*cos(((double)j/(double)slices)*2*pi);
-			points[i][j].y=r*sin(((double)j/(double)slices)*2*pi);
-			points[i][j].z=h;
+			// create a plane above the xy plane at point z = 1; 
+			points[i][j].x= -1 +  (double)i/slices*2 ;
+			points[i][j].y= -1 +  (double)j/slices*2 ;
+			points[i][j].z=1;
+
+			// because of normalization, the points are now on the surface of a sphere
+			points[i][j] = points[i][j].normalize_vector();
+			// now multiply the points with the radius to get the desired radius	
+			points[i][j] = points[i][j]*radius;
 		}
 	}
 	//draw quads using generated points
-	for(i=0;i<stacks;i++)
+	for(int i=0;i<slices;i++)
 	{
-        glColor3f((double)i/(double)stacks,(double)i/(double)stacks,(double)i/(double)stacks);
-		for(j=0;j<slices;j++)
+        // glColor3f((double)i/(double)stacks,(double)i/(double)stacks,(double)i/(double)stacks);
+		for(int j=0;j<slices;j++)
 		{
 			glBegin(GL_QUADS);{
 			    //upper hemisphere
@@ -177,18 +146,68 @@ void drawSphere(double radius,int slices,int stacks)
 				glVertex3f(points[i][j+1].x,points[i][j+1].y,points[i][j+1].z);
 				glVertex3f(points[i+1][j+1].x,points[i+1][j+1].y,points[i+1][j+1].z);
 				glVertex3f(points[i+1][j].x,points[i+1][j].y,points[i+1][j].z);
-                //lower hemisphere
-                glVertex3f(points[i][j].x,points[i][j].y,-points[i][j].z);
-				glVertex3f(points[i][j+1].x,points[i][j+1].y,-points[i][j+1].z);
-				glVertex3f(points[i+1][j+1].x,points[i+1][j+1].y,-points[i+1][j+1].z);
-				glVertex3f(points[i+1][j].x,points[i+1][j].y,-points[i+1][j].z);
 			}glEnd();
 		}
 	}
 }
 
+void drawSphere()
+{
+	for(int i=0;i<4;i++){
+        glPushMatrix();{
+            glColor3f(0, (i+1)%2, i%2);     
+            glRotatef(90*i,0,1,0);
+            glTranslatef(0,0,edge_len);
+            drawSpherePart(current_radius,100);
+        }glPopMatrix();
+    }
+
+	for(int i=0;i<2;i++){
+        glPushMatrix();{
+            glColor3f(1,0,0);     
+            glRotatef(90+180*i,1,0,0);
+            glTranslatef(0,0,edge_len);
+            drawSpherePart(current_radius,100);
+        }glPopMatrix();
+    }
+}
+
+void drawCylinder(){
+	
+	// 8 cylinder parts for 8 edges of the octahedorn
+
+	//upper part
+	for(int i = 0; i<4; i++){
+		glPushMatrix();{
+			glRotatef(45+90*i, 0, 1, 0);
+			glTranslatef(edge_len/sqrt(2), 0, 0);
+			drawCylinderPart(current_radius, edge_len*sqrt(2), 100);	
+		}glPopMatrix();
+	}
+
+	// lower part
+	for(int i = 0; i<4; i++){
+		glPushMatrix();{
+			glRotatef(90, 0, 0, 1);
+			glRotatef(45+90*i, 0, 1, 0);
+			glTranslatef(edge_len/sqrt(2), 0, 0);
+			drawCylinderPart(current_radius, edge_len*sqrt(2), 100);	
+		}glPopMatrix();
+	}
+
+	// on the xy plane
+	for(int i = 0; i<4; i++){
+		glPushMatrix();{	
+			glRotatef(45+90*i, 0, 0, 1);
+			glRotatef(90, 1, 0, 0);
+			glTranslatef(edge_len/sqrt(2), 0, 0);
+			drawCylinderPart(current_radius, edge_len*sqrt(2), 100);
+		}glPopMatrix();
+	}
+}
+
+
 void keyboardListener(unsigned char key, int x,int y){
-	// cout<<"Current Camera position";camera->position->print_vector();
 	switch(key){
 
 		case '1':
@@ -215,6 +234,27 @@ void keyboardListener(unsigned char key, int x,int y){
 		case 's':
 			camera->down_reference();
 			break;
+		case '.':
+			edge_len += 0.1;
+			current_radius -= increment_radius;
+			if(edge_len > max_edge_len){
+				edge_len = max_edge_len;
+				current_radius = 0.0;
+			}
+			break;
+		case ',':
+			edge_len -= 0.1;
+			current_radius += increment_radius;
+			if(current_radius > max_radius){
+				current_radius = max_radius;
+				edge_len = 0.0;
+			}
+			break;
+
+		case ' ':
+			cout<<"Current Camera position";camera->position->print_vector();
+			break;
+
 		
 		default:
 			break;
@@ -288,7 +328,10 @@ void display(){
 
 	drawAxes();
 	drawGrid();
-    drawTriangles();
+    
+	drawTriangles();
+	drawSphere();
+	drawCylinder();
 
 	//ADD this line in the end --- if you use double buffer (i.e. GL_DOUBLE)
 	glutSwapBuffers();
@@ -301,18 +344,20 @@ void animate(){
 
 void init(){
 	//codes for initialization
-	drawgrid=1;
-	drawaxes=1;
-    triangle_edge_len =  1.6;
-
-    //The formula for the radius of the inscribed sphere\
+	drawgrid=0;
+	drawaxes=0;
+    
+	edge_len = 1.6;
+	max_edge_len = 1.6;
+    
+	//The formula for the radius of the inscribed sphere\
     in a regular tetrahedron is edgeLength / sqrt(3.0)
-    max_sphere_radius = triangle_edge_len / sqrt(3.0);
-    current_sphere_radius = 0.0;
-    increment_radius = max_sphere_radius / 16;        // 16 steps to reach the maximum radius
+    max_radius = edge_len / sqrt(3.0);
+    current_radius = 0.0;
+    increment_radius = max_radius / 16;        // 16 steps to reach the maximum radius
     
     camera = new Camera(
-        new Vector3D(0, -15.907796, 9.451017),
+        new Vector3D(-0.127056, -4.212026, 2.277302),
         new Vector3D(0, 0, 0),
         new Vector3D(0, 1, 0)
     );
--- Offline 1/run.sh
+++ Offline 1/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 
-# g++ demo4.cpp -o demo -lglut -lGLU -lGL
 g++ play.cpp -o demo -lglut -lGLU -lGL
 ./demo
